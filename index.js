f7ccceedc91ff0cc00b7baedf92e527ee34f0f0d
AHM-Moinuddin-Sami <moinuddinsami1205@gmail.com>
2023-05-20 19:08:41 +0600

    added search functionality

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xothnon.mongodb.net/?retryWrites=true&w=majority`;
+let query = {};
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,12 +29,31 @@ async function run() {
         const toysCollection = client.db("PlaytopiaDB").collection("toys");
 
         app.get('/toys', async (req, res) => {
+            const {page, limit, searchedToy} = req.query;
+            const currPage = parseInt(page) || 0;
+            const pageLimit = parseInt(limit) || 20;
+            const skip = currPage * pageLimit;
 
-            const page = parseInt(req.query.page) || 0;
-            const limit = parseInt(req.query.limit) || 20;
-            const skip = page * limit;
+            if(searchedToy){
+                query = { toyName: { $regex: searchedToy, $options: 'i' } };
+            }
+            else{
+                query = {};
+            }
 
-            const result = await toysCollection.find().limit(limit).skip(skip).toArray();
+            console.log(currPage, pageLimit, query);
+
+            const result = await toysCollection.find(query).limit(pageLimit).skip(skip).toArray();
+            res.send(result);
+        })
+
+        app.get('/toys/:id', async (req, res) => {
+            const id = req.params.id;
+
+            const query = { _id: new ObjectId(id) }
+
+            const result = await toysCollection.findOne(query);
+
             res.send(result);
         })
 
